2c939bbcdcfe6d00af63f92751b207287dc3b301
Hrutik Mehta <hrutikmehta@gmail.com>
2023-06-01 16:00:24 -0400

    Built a database and added cog wlboard.py

--- W-LBoard.py
+++ W-LBoard.py
@@ -1,4 +1,6 @@
-from lib.bot import wlboard
-
-VERSION = "0.0.1"
-handoftheking.run(VERSION)
+from lib.bot import WLBoard
+guild = 546243371602149406
+channels ={'wlboard': 1113648494096420944, 'botSpam': 774440654465269781}
+wlboard = WLBoard(guild, channels)
+#VERSION = "0.0.1"
+wlboard.run()
---
+++ data/db/build.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS wlboard(
+    RootMessageID integer PRIMARY KEY,
+    WLMessageID integer,
+    Stars integer Default 1
+);
